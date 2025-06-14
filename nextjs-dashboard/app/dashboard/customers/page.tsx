40981794fba60f0c9d2e3bb99730ad9824448ebe
MarcosJanczeski <marcosjanczeski@gmail.com>
2025-06-14 17:16:34 -0400

    EM 14/06/2025

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
+export default async function Page() {
+  console.log('Fetching revenue data...');
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+  
+      console.log('Customers data fetch completed after 3 seconds.');
   return <p>Customers Page</p>;
 }
